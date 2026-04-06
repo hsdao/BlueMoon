55a3e58301939a9c1f39eb99c8073f3fd7959e18
m. duong <duongtuyetmai2802@gmail.com>
2026-04-06 21:32:26 +0700

    fix lỗi, giao diện, logic code; thêm form add/edit

---
+++ src/main/java/services/db/MysqlConnection.java
@@ -0,0 +1,28 @@
+package services.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MysqlConnection {
+    private static Connection connection;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/bluemoon?useUnicode=true&characterEncoding=utf-8";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private MysqlConnection() {}
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Lỗi cấu hình kết nối Database!");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
